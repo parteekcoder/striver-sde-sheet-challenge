f86268ead763835361e44c127d5e2d0c66d82d34
parteekcoder <parteekgoyal118@gmail.com>
2023-06-05 21:38:55 +0530

    first day

--- Arrays/next_permutation.cpp
+++ Arrays/next_permutation.cpp
@@ -7,31 +7,52 @@ using namespace std;
 class Solution
 {
 public:
-    void nextPermutation(vector<int> &nums)
-    {
+    // void nextPermutation(vector<int> &nums)
+    // {
+    //     int n = nums.size();
+    //     bool isCompleted = true;
+    //     for (int i = n - 2; i >= 0; i--)
+    //     {
+    //         int minGreater = INT_MAX;
+    //         int minIndex = -1;
+    //         for (int j = i + 1; j < n; j++)
+    //         {
+    //             if (minGreater > nums[j] && nums[j] > nums[i])
+    //             {
+    //                 minGreater = nums[j];
+    //                 minIndex = j;
+    //             }
+    //         }
+    //         if (minIndex == -1)
+    //             continue;
+    //         swap(nums[i], nums[minIndex]);
+    //         sort(nums.begin() + i + 1, nums.end());
+    //         isCompleted = false;
+    //         break;
+    //     }
+    //     if (isCompleted)
+    //         sort(nums.begin(), nums.end());
+    // }
+    void nextPermutation(vector<int>&nums){
         int n = nums.size();
-        bool isCompleted = true;
-        for (int i = n - 2; i >= 0; i--)
-        {
-            int minGreater = INT_MAX;
-            int minIndex = -1;
-            for (int j = i + 1; j < n; j++)
-            {
-                if (minGreater > nums[j] && nums[j] > nums[i])
-                {
-                    minGreater = nums[j];
-                    minIndex = j;
-                }
+        int index = -1;
+        for(int i=n-2;i>=0;i--){
+            if(nums[i]<nums[i+1]){
+                index=i;
+                break;
             }
-            if (minIndex == -1)
-                continue;
-            swap(nums[i], nums[minIndex]);
-            sort(nums.begin() + i + 1, nums.end());
-            isCompleted = false;
-            break;
         }
-        if (isCompleted)
-            sort(nums.begin(), nums.end());
+        if(index==-1){
+            reverse(nums.begin(),nums.end());
+            return;
+        }
+        for(int i=n-1;i>=0;i--){
+            if(nums[i]>nums[index]){
+                swap(nums[i],nums[index]);
+                break;
+            }
+        }
+        reverse(nums.begin()+index+1,nums.end());
     }
 };
 int main()
